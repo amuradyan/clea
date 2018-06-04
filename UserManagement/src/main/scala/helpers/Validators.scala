8504f082c267b04f84ea6510fb35bae1f0b0395d
Andranik Muradyan <muradianator@gmail.com>
2018-06-04 20:56:53 +0400

    Moved bots, roles and regions to config file

--- UserManagement/src/main/scala/helpers/Validators.scala
+++ UserManagement/src/main/scala/helpers/Validators.scala
@@ -1,20 +1,27 @@
 package helpers
 
+import com.typesafe.config.ConfigFactory
+
 /**
   * Created by spectrum on 6/4/2018.
   */
 object Validators {
+  lazy val conf = ConfigFactory.load()
+  lazy val bots = conf.getStringList("app.bots")
+  lazy val regions = conf.getStringList("app.regions")
+  lazy val roles = conf.getStringList("app.roles")
+
   def isValidSource(source: String): Boolean = isValidBotName(source) || source.equalsIgnoreCase("manual")
 
   def isEmail(email: String) = """(\w+)@([\w\.]+)""".r.unapplySeq(email.toLowerCase).isDefined
 
   def isPhoneNumber(phoneNumber: String) = """^\+\d{5,17}$""".r.unapplySeq(phoneNumber.toLowerCase).isDefined
 
-  def isRegion(region: String) = Seq("arm", "rus", "ukr", "kaz").contains(region.toLowerCase)
+  def isRegion(region: String) = regions.contains(region.toLowerCase)
 
-  def isRole(role: String) = Seq("admin", "manager", "client").contains(role.toLowerCase)
+  def isRole(role: String) = roles.contains(role.toLowerCase)
 
-  def isValidBotName(botName: String) = Seq("alpinist", "cleversniper").contains(botName.toLowerCase)
+  def isValidBotName(botName: String) = bots.contains(botName.toLowerCase)
 
   def isValidBookName(bookName: String) =  "profit".equals(bookName) || isValidBotName(bookName)
 
